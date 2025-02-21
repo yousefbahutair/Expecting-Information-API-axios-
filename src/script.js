599103f1ae2f1cc31c3cd3191aeb698124595729
yousefbahutair <yousefnas06@gmail.com>
2025-02-21 10:22:35 +0400

    refactor(api) : improve axios promise handling

--- src/script.js
+++ src/script.js
@@ -1,16 +1,26 @@
-axios.get('https://swapi.dev/api/planets/')
-.then(({data}) => {
-    for( planet of data){
-    console.log('PLANET NAME: ', planet.name);
+const fetchNextPlanets = (url) => {
+    console.log(url);
+    return axios.get(url);
+}
+const printPlanets = ({data}) => {
+    console.log(data);
+    for(let planet of data){
+        console.log("PLANET NAME:", planet.name)
     }
-    return axios.get(data.next);
-})
-.then(({data}) => {
-    console.log('ANOTHER PLANETS:\n')
-    for(planet of data){
-        console.log('PLANET NAME:', planet.name);
+    return Promise.resolve(data.next)
+}
+const morePlanets = ({data}) => {
+    console.log(data);
+    console.log("ANOTHER PLANETS:\n");
+    for(let planet of data){
+        console.log("PLANET NAME:", planet.name)
     }
-})
+}
+
+fetchNextPlanets('https://swapi.dev/api/planets/')
+.then(printPlanets)
+.then(fetchNextPlanets)
+.then(morePlanets)
 .catch((err) => {
     console.log('AN ERROR OCURR', err);
 })
